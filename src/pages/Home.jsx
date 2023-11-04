222ae705a023e850735cba1615946ccdf3bed536
Louis Marc Leonard <llouismrc2@gmail.com>
2023-11-03 20:25:19 -0400

    .................

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/hero/Hero'
+import Hero from '../components/hero/Hero.jsx'
 import Slider from '../components/slider/Slider'
 import Virtual from '../components/virtual/Virtual'
 import ProductsPage from '../components/products/ProductsPage'
