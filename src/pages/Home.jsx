7c8ae59111da2b0864f10c39013c52c3137a67d8
Louis Marc Leonard <llouismrc2@gmail.com>
2023-11-03 20:31:02 -0400

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Hero from '../components/hero/Hero.jsx'
-import Slider from '../components/slider/Slider'
-import Virtual from '../components/virtual/Virtual'
-import ProductsPage from '../components/products/ProductsPage'
-import Testimonial from '../components/testimonial/Testimonial'
+import React from 'react';
+import Hero from '../components/hero/Hero';
+import Slider from '../components/slider/Slider';
+import Virtual from '../components/virtual/Virtual';
+import ProductsPage from '../components/products/ProductsPage';
+import Testimonial from '../components/testimonial/Testimonial';
 
 
 
